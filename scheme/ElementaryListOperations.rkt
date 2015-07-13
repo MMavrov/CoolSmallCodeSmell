f98e876032816b7919700b5ea6849c52deae6bd4
Milen Mavrov <m.mavr0v@yahoo.com>
2015-07-13 13:28:56 +0300

    deep_count and higher order functions

--- scheme/ElementaryListOperations.rkt
+++ scheme/ElementaryListOperations.rkt
@@ -26,3 +26,43 @@
     ((null? l) #f)
     ((equal? x (car l)) #t)
     (else (member x (cdr l)))))
+
+;a variant of the deep_count function where the empty list is NOT counted as atom
+(define (atom? x)
+  (not (or (pair? x) (null? x) (vector? x))))
+
+(define (deep_count_atoms l)
+  (cond
+    ((null? l) 0)
+    ((atom? l) 1)
+    (else (+ (deep_count_atoms (car l)) (deep_count_atoms (cdr l))))))
+
+;a variant of the deep_count function where the empty list is counted as atom
+(define (atom_including_empty_list? x)
+  (not (or (pair? x) (vector? x))))
+
+(define (deep_count_atoms_including_empty_list l)
+  (cond
+    ((null? l) 0)
+    ((atom_including_empty_list? (car l)) (+
+                                 1
+                                 (deep_count_atoms_including_empty_list (cdr l))))
+    (else (+
+           (deep_count_atoms_including_empty_list (car l))
+           (deep_count_atoms_including_empty_list (cdr l))))))
+
+;higher order functions
+(define (accumulate l operation initial_value)
+  (if (null? l) initial_value
+      (operation (car l) (accumulate (cdr l) operation initial_value))))
+
+(define (my_map l f)
+  (if (null? l) l
+      (cons (f (car l)) (my_map (cdr l) f))))
+
+(define (filter l predicate)
+  (cond
+    ((null? l) l)
+    ((predicate (car l)) (cons (car l) (filter (cdr l) predicate)))
+    (else (filter (cdr l) predicate))))
+
